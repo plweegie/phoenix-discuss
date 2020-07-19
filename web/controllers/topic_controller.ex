5b453c9349b906a6415840c23c256052c49641ea
Jan Szymanski <jan.kazimierz.szymanski@gmail.com>
2020-07-19 20:45:29 +0100

    Add create topic flow

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,14 +3,27 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"topic" => topic}) do
-    
-  end
+    changeset = Topic.changeset(%Topic{}, topic)
 
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+  end
 end
